c3cc2642e8e201d9e1780e286bbdaa967ea243be
tonisancho5 <tonisancho5@gmail.com>
2022-10-17 08:40:43 +0000

    empires of legends

---
+++ .bash_history
@@ -0,0 +1,124 @@
+clean
+pwd
+clear
+ls -la
+mkdir modules
+cd modules
+odoo scaffold proves .
+ls
+odoo --addons-path="/var/lib/odoo/modules,/usr/lib/python3/dist-packages/odoo/addons" --save
+cd
+cat .odoorc 
+service odoo stop
+odoo
+exit
+service odoo status
+odoo
+service odoo stop
+service odoo status
+cd modules
+ls
+odoo scaffold school
+cd
+odoo
+odoo
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+git init
+git config --global user.name "tonisancho5"
+git config --global user.email "tonisancho5@gmail.com"
+git remote add origin https://github.com/tonisancho5/sge22-23.git
+git pull origin master
+git pull origin master
+git add *
+git commit -m "proves git"
+git push origin master
+git push origin master
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+odoo -d sge22 -u school
+cd modules
+odoo scaffold empiresOfLegends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+git add *
+git commit -m "proves git"
+git push origin master
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+service odoo stop
+odoo -d sge22 -u empires_of_legends
+ps aux | grep odoo
+kill 30179
+odoo -d sge22 -u empires_of_legends
+service odoo stop
+ps aux | grep odoo
+kill 30179
+ps aux | grep odoo
+kill 30179
+ps aux | grep odoo
+kill -9 30179
+ps aux | grep odoo
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo -d sge22 -u empires_of_legends
+odoo 
--- modules/empires_of_legends/models/models.py
+++ modules/empires_of_legends/models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from email.policy import default
 from odoo import models, fields, api
 
 
@@ -7,20 +8,101 @@ class player(models.Model):
     _name = 'empires_of_legends.player'
     _description = 'Players'
 
-    name = fields.Char(required =True)
-    avatar = fields.Image(max_width = 200, max_height=200)
+    name = fields.Char(required=True, default="Player Name")
+    password = fields.Char()
+    avatar = fields.Image(max_width=200, max_height=200)
+    villages = fields.One2many('empires_of_legends.village', 'player')
+
 
 class territory(models.Model):
     _name = 'empires_of_legends.territory'
     _description = 'Territories'
 
-    name = fields.Char(required = True)
+    name = fields.Char(required=True)
+    villages = fields.One2many('empires_of_legends.village', 'territory')
+    resourceSites = fields.One2many('empires_of_legends.resource_site', 'territory')
+    
+
 
 class village(models.Model):
     _name = 'empires_of_legends.village'
     _description = 'Villages'
 
-    name = fields.Char(required = True)
+    name = fields.Char(required=True)
+    civilization = fields.Selection([('1', 'Bizantinos'), ('2', 'Britanos'), ('3', 'Celtas'),
+                                     ('4', 'Chinos'), ('5', 'Francos'), ('6', 'Godos'), ('7', 'Japoneses')])
+    food = fields.Integer()
+    wood = fields.Integer()
+    stone = fields.Integer()
+    iron = fields.Integer()
+    gold = fields.Integer()  # de pago
+
+    player = fields.Many2one('empires_of_legends.player')
+    territory = fields.Many2one('empires_of_legends.territory')
+    buildings = fields.One2many('empires_of_legends.building', 'village')
+    troops_qty = fields.One2many('empires_of_legends.troop_qty', 'village')
+    
+
+class resource_site(models.Model):
+    _name = 'empires_of_legends.resource_site'
+    _description = 'Resource Sites'
+
+    name = fields.Char(required=True, default="Resource Site")
+    resourceType = fields.Selection([('1', 'Food'), ('2', 'Wood'), ('3', 'Stone'), ('4', 'Iron')])
+    territory = fields.Many2one('empires_of_legends.territory')
 
 
+class building(models.Model):
+    _name = 'empires_of_legends.building'
+    _description = 'Building'
 
+    name = fields.Char()
+
+    produce_food = fields.Float(related='type.produce_food')
+    produce_wood = fields.Float(related='type.produce_wood')
+    produce_stone = fields.Float(related='type.produce_stone')
+    produce_iron = fields.Float(related='type.produce_iron')
+
+    consume_food = fields.Float(related='type.consume_food')
+    consume_wood = fields.Float(related='type.consume_wood')
+    consume_stone = fields.Float(related='type.consume_stone')
+    consume_iron = fields.Float(related='type.consume_iron')
+
+    train_infantry = fields.Integer(related='type.train_infantry')
+    train_cavalry = fields.Integer(related='type.train_cavalry')
+    train_archery = fields.Integer(related='type.train_archery')
+    train_siege = fields.Integer(related='type.train_siege')
+
+    troops_qty = fields.Integer(related='type.troops_qty')
+
+    type = fields.Many2one('empires_of_legends.building_type')
+    village = fields.Many2one('empires_of_legends.village')
+
+
+class building_type(models.Model):
+    _name = 'empires_of_legends.building_type'
+    _description = 'Building Types'
+
+    name = fields.Char()
+
+    produce_food = fields.Float()
+    produce_wood = fields.Float()
+    produce_stone = fields.Float()
+    produce_iron = fields.Float()
+
+    consume_food = fields.Float()
+    consume_wood = fields.Float()
+    consume_stone = fields.Float()
+    consume_iron = fields.Float()
+
+    train_infantry = fields.Integer()
+    train_cavalry = fields.Integer()
+    train_archery = fields.Integer()
+    train_siege = fields.Integer()
+    troops_qty = fields.Integer()
+
+class troop_qty(models.Model):
+    _name = 'empires_of_legends.troop_qty'
+    _description = 'Troop quantity'
+
+    village = fields.Many2one('empires_of_legends.village')
